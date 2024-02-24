2c8b9bcbc3fb1ccd1ec78b254328e306cf460995
youssef marzoug <youssefmarzoug19@gmail.com>
2024-02-24 23:28:21 +0100

    seccess sreens

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import orderRoutes from './routes/orderRoutes.js';
 
 connectToDatabase();
 const app = express();
-app.use(express.json());
+app.use(express.json()); 
 app.use(cors());
 
 app.use('/api/products', productRoutes);
