38b8e7af3dcc1bf32687c6ee524d834c34247cea
youssef marzoug <125292810+yousssefmarzoug@users.noreply.github.com>
2024-03-15 18:42:51 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,11 +29,11 @@ const port = 5000;
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV == 'production') {
+
 	app.use(express.static(path.join(__dirname, '/client/build')));
 
 	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+
 
 app.get('/', (req, res) => {
 	res.send('Api is running...');
